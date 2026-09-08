82cf856ed93b0e84b9d4025f0af2c917b96fa1b8
Ritesh Kumar <kumarritesh26579@gmail.com>
2026-09-08 16:52:03 +0530

    style: apply exact original bhrosacab.com layout, grid and responsive styling

--- client/src/components/layout/MainLayout/MainLayout.jsx
+++ client/src/components/layout/MainLayout/MainLayout.jsx
@@ -5,17 +5,18 @@ import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
 import StickyNavbar from "../StickyNavbar/StickyNavbar";
 import Footer from "../Footer/Footer";
+import "./MainLayout.css";
 
 function MainLayout() {
   return (
-    <>
+    <div className="site-wrapper">
       <TopHeader />
       <Header />
       <Navbar />
       <StickyNavbar />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
 
